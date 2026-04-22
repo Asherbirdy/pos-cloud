09d5682a7fc513d60f737401c0ea174eb101084f
Asher Lien <birdym.tw@gmail.com>
2026-04-22 15:07:37 +0800

    wip

---
+++ src/main/java/com/app/security/model/Store.java
@@ -0,0 +1,60 @@
+package com.app.security.model;
+
+import java.util.Date;
+
+public class Store {
+    private String storeId;
+    private String enterpriseId;
+    private String name;
+    private Boolean isActive;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public String getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(String storeId) {
+        this.storeId = storeId;
+    }
+
+    public String getEnterpriseId() {
+        return enterpriseId;
+    }
+
+    public void setEnterpriseId(String enterpriseId) {
+        this.enterpriseId = enterpriseId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
