3fb8244e12d6d216c3b913ba0121ae0c930df24d
Asher Lien <birdym.tw@gmail.com>
2026-04-22 10:02:25 +0800

    model

---
+++ src/main/java/com/app/security/model/Enterprise.java
@@ -0,0 +1,42 @@
+package com.app.security.model;
+
+import java.util.Date;
+
+public class Enterprise {
+    private String companyId;
+    private String name;
+    private Date createdAt;
+    private Date updatedAt;
+
+    public String getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(String companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
