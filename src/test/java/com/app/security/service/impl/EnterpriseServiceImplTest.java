652ce897a209087ba062d686c17b70d525bb3da5
Asher Lien <birdym.tw@gmail.com>
2026-04-23 10:41:51 +0800

    asd

---
+++ src/test/java/com/app/security/service/impl/EnterpriseServiceImplTest.java
@@ -0,0 +1,16 @@
+package com.app.security.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class EnterpriseServiceImplTest {
+
+    @Test
+    public void checkRolePermit (){
+
+
+    }
+}
