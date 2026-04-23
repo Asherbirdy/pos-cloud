cfa7c9e9013177234de1568b686dbda2f4859e84
Asherbirdy <122598799+Asherbirdy@users.noreply.github.com>
2026-04-23 10:38:37 +0800

    Feat/h2和unit test設定 (#3)
    * asd
    
    * asd
    
    * aasd

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,16 +1,8 @@
--- 密碼的值為「帳號去掉 @gmail.com」，例如 admin@gmail.com 的密碼為 admin
-INSERT INTO member(email, password, name, age) VALUES ('admin@gmail.com', '$2a$10$DuMH/aNBaYtHT0j/b2swG.0Z7eYJ8Sckw06PSdbY4Kqy4PzZxQJeu', 'Admin', '20');
-INSERT INTO member(email, password, name, age) VALUES ('normal@gmail.com', '$2a$10$B/OtT9gtlgsh/w8tdO.ISeFHNNzcILIgRuh2vDwFRUSPGRC.WqWRq', 'Normal Member', '30');
-INSERT INTO member(email, password, name, age) VALUES ('vip@gmail.com', '$2a$10$/cAcYaD3L4cvf09pC4YwkOB.KHPi.JzAXG1qoqtTVniaOeDog5Atm', 'VIP Member', '22');
-INSERT INTO member(email, password, name, age) VALUES ('movie-manager@gmail.com', '$2a$10$4ygMq.sezGuSU7UxNPiVwefcWSVZq17z98MrTK1KhsO2AMQU1qbDa', 'Movie Manager', '25');
+-- 密碼的值為 password
+INSERT INTO member (member_id, name, email, password, role, created_at, updated_at)
+VALUES
+    ('a5ff12fd-f83c-4243-8445-7b59f8dcf8ed','AdminAccount','admin@gmail.com','$2a$10$x5UYNqh6Tz76Ay0UdRk2uuD20hc367bmmIWFLrSjtPs5qa1c2AcKy','admin','2026-04-22 10:40:56.196950','2026-04-22 10:40:56.196950'),
+    ('b6aa23fd-f83c-4243-8445-7b59f8dcf8ed','UserAccount','user@gmail.com','$2a$10$x5UYNqh6Tz76Ay0UdRk2uuD20hc367bmmIWFLrSjtPs5qa1c2AcKy','user','2026-04-22 10:40:56.196950','2026-04-22 10:40:56.196950');
 
-INSERT INTO role(role_name) VALUES ('ROLE_ADMIN');
-INSERT INTO role(role_name) VALUES ('ROLE_NORMAL_MEMBER');
-INSERT INTO role(role_name) VALUES ('ROLE_VIP_MEMBER');
-INSERT INTO role(role_name) VALUES ('ROLE_MOVIE_MANAGER');
 
-INSERT INTO member_has_role (member_id, role_id) VALUES (1, 1);
-INSERT INTO member_has_role (member_id, role_id) VALUES (2, 2);
-INSERT INTO member_has_role (member_id, role_id) VALUES (3, 2);
-INSERT INTO member_has_role (member_id, role_id) VALUES (3, 3);
-INSERT INTO member_has_role (member_id, role_id) VALUES (4, 4);
+INSERT INTO enterprise (enterprise_id, name) VALUES ('fdda207b-e2be-4384-aa1c-6e2bcf1e5a16','好愛我企業');
