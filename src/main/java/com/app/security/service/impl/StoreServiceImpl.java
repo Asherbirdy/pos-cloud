48fa7545d2f7b88ef85546a0b93fc45225336d7b
Asher Lien <birdym.tw@gmail.com>
2026-04-23 17:12:01 +0800

    更新

--- src/main/java/com/app/security/service/impl/StoreServiceImpl.java
+++ src/main/java/com/app/security/service/impl/StoreServiceImpl.java
@@ -3,7 +3,9 @@ package com.app.security.service.impl;
 import com.app.security.dao.StoreDao;
 import com.app.security.model.Store;
 import com.app.security.service.StoreService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -28,6 +30,11 @@ public class StoreServiceImpl implements StoreService {
 
     @Override
     public void edit(String storeId, String name, Boolean isActive, Integer runningDevicesLimit) {
+
+        if (runningDevicesLimit > 10) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "RUNNING_DEVICES_LIMIT_ERROR");
+        }
+
         storeDao.editStore(storeId, name, isActive, runningDevicesLimit);
     }
 }
