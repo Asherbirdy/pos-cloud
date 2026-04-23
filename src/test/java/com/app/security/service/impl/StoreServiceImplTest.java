d3362a42e0a95ebc813cc82e6877265b0bee42ff
Asher Lien <birdym.tw@gmail.com>
2026-04-23 17:23:30 +0800

    asd

---
+++ src/test/java/com/app/security/service/impl/StoreServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.app.security.service.impl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class StoreServiceImplTest {
+
+    @Test
+    public void test() {}
+
+}
